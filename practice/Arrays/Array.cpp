555cac9077f3c45b1339daf20b30d72ac17eb940
anujithpp <anujithpp2912004@gmail.com>
2024-02-04 19:59:19 +0530

    updated array

--- practice/Arrays/Array.cpp
+++ practice/Arrays/Array.cpp
@@ -43,6 +43,63 @@ void Array<T>::insert_at_position(T key, int pos)
 	A[pos] = key;
 }
 
+template <class T>
+void Array<T>::dlt_at_end()
+{
+	UB -= 1;
+}
+
+template <class T>
+void Array<T>::dlt_at_begining()
+{
+	LB += 1;
+}
+
+template <class T>
+void Array<T>::dlt_at_index(int pos)
+{
+	if (LB<=pos and UB>=pos)
+	{
+		k = pos + 1;
+		while (k <= UB)
+		{
+			A[k - 1] = A[k];
+			k += 1;
+		}
+		UB -= 1;
+	}
+	else
+	{
+		cout << "out of boundry";
+	}
+}
+
+template<class T>
+T Array<T>::linear_search(T key)
+{
+	int index=NULL;
+	int i=LB;
+	while (i<=UB)
+	{
+		if (A[i]==key)
+		{
+			index=i;
+			break;
+		}
+		i=i+1;
+	}
+	return index;
+}
+
+template<class T>
+T Array<T>::swap(T,int p,int q)
+{
+	int t=A[p];
+	A[p]=A[q];
+	A[q]=t;
+}
+
+
 template <class U>
 ostream &operator<<(ostream &os, Array<U> M)
 {
--- practice/Arrays/Array.h
+++ practice/Arrays/Array.h
@@ -15,12 +15,14 @@ public:
 	void insert_at_begining(T);
 	void insert_at_position(T, int);
 	void dlt_at_begining();
-	void dlt_at_end();
-	void dlt_at_index();
+    void dlt_at_index(int pos);
+    void dlt_at_end();
+    void dlt_at_index();
 
 	T linear_search(T);
-	T swap();
-	T find_min();
+    T swap(T, int p, int q);
+    T swap();
+    T find_min();
 	T selection_sort();
 	T binary_search(T);
 
