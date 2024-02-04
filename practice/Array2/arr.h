a22141d9e45dfc386b56b656bb4604b555335b8a
anujithpp <anujithpp2912004@gmail.com>
2024-02-04 10:51:45 +0530

    some bugs fixed

--- practice/Array2/arr.h
+++ practice/Array2/arr.h
@@ -7,7 +7,7 @@ public:
     void printArray(int a[], int n)
     {
         int i;
-        for (i = 0; i <= n; i++)
+        for (i = 0; i < n; i++)
         {
             cout << a[i] << ",";
         }
