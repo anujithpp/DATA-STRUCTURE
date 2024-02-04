c9808bf79865c58c93c5ecfc850230a37dac8403
anujithpp <anujithpp2912004@gmail.com>
2024-02-04 10:49:15 +0530

    added new array methods

---
+++ practice/Array2/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "arr.h"
+using namespace std;
+
+int main()
+{
+    int Arr[4] = {1, 2, 3, 4};
+    Array obj1;
+    obj1.printArray(Arr, 4);
+    return 0;
+}
---
+++ practice/Array2/arr.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Array
+{
+public:
+    void printArray(int a[], int n)
+    {
+        int i;
+        for (i = 0; i <= n; i++)
+        {
+            cout << a[i] << ",";
+        }
+    }
+};
