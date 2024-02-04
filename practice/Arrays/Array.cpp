5d607e1154d1f2284905206b855213f84fdf75b8
anujithpp <anujithpp2912004@gmail.com>
2024-02-04 16:38:09 +0530

    updated array

--- practice/Arrays/Array.cpp
+++ practice/Arrays/Array.cpp
@@ -1,27 +1,44 @@
-#include"Array.h"
+#include "Array.h"
 
+template <class T>
+Array<T>::Array()
+{
+	LB = 1;
+	UB = 0;
+}
+template <class T>
+Array<T>::Array(int LB, int UB, T x[])
+{
+}
+template <class T>
+void Array<T>::insert_at_end(T key)
+{
+	UB = UB + 1;
+	A[UB] = key;
+}
 
 template <class T>
-Array<T>::Array(){
-	LB=1;
-	UB=0;
-}
-template <class T>
-Array<T>::Array(int LB,int UB,T x[]){
-	
-}
-template <class T>
-void Array<T>::insert_at_end(T key){
-	UB=UB+1;
-	A[UB]=key;
-}
-template <class U>
-ostream& operator << (ostream& os,Array<U> M){
-	int i;
-	os<<endl;
-	for (i=M.LB;i<=M.UB;i++){
-		os<<M.A[i]<<" ";
+void Array<T>::insert_at_begining(T key)
+{
+	UB += 1;
+	int i = UB - 1;
+	while (i >= LB)
+	{
+		A[i + 1] = A[i];
+		i -= 1;
 	}
-	os<<endl;
+	A[LB] = key;
+}
+
+template <class U>
+ostream &operator<<(ostream &os, Array<U> M)
+{
+	int i;
+	os << endl;
+	for (i = M.LB; i <= M.UB; i++)
+	{
+		os << M.A[i] << " ";
+	}
+	os << endl;
 	return os;
 }
--- practice/Arrays/Array.h
+++ practice/Arrays/Array.h
@@ -1,15 +1,29 @@
-#include<iostream>
-#include<ostream>
-
+#include <iostream>
+#include <ostream>
 using namespace std;
 template <class T>
-class Array{
-	int LB,UB;
+
+class Array
+{
+	int LB, UB;
 	T A[100];
-  public:
-  	Array();
-  	Array(int,int,T[]);
-  	void insert_at_end(T);
- 	template <class U>
-  	friend ostream& operator<<(ostream&, Array<U> );
+
+public:
+	Array();
+	Array(int, int, T[]);
+	void insert_at_end(T);
+	void insert_at_begining(T);
+	void insert_at_position(T, int);
+	void dlt_at_begining();
+	void dlt_at_end();
+	void dlt_at_index();
+
+	T linear_search(T);
+	T swap();
+	T find_min();
+	T selection_sort();
+	T binary_search(T);
+
+	template <class U>
+	friend ostream &operator<<(ostream &, Array<U>);
 };
